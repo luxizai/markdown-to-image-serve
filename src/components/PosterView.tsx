fec222d52b3519db069a6548250117243b52d1d3
wxingheng <wxingheng@outlook.com>
2024-11-29 20:53:27 +0800

    feat: poster

--- src/components/PosterView.tsx
+++ src/components/PosterView.tsx
@@ -30,13 +30,6 @@ export default function PosterView() {
 
   return (
     <div className="poster-content" style={{display: "inline-block"}}>
-      <style jsx>{`
-       @font-face {
-          font-family: 'Noto Sans SC';
-          src: url('/NotoSansSC-Regular.otf') format('opentype');
-        }
-      `}</style>
-      <span>asd发售的客服了解</span>
           {/* Preview */}
             <Md2Poster theme="SpringGradientWave" >
               <Md2PosterHeader  className="flex justify-center items-center px-4 font-medium text-lg">
